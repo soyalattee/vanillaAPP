c9e325f7c993bf0f9c5e51d7c495b040596a3d3e
soyalatte <soyalatte@neowiz.com>
2022-06-03 12:12:44 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
+
+// string만 포함된 변수는 대문자로 표기!
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  //기본 동작을 막아줌. 새로고침을 막아준다!
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  //get event info, 자동으로 이벤트를 함수에 첫번째 인자로 전달해준다.
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `hello  ${username}`;
+}
